d5485cdae9438f7b44c3f7eaa02521a0aaf53be7
Beknur <d.zheenkul@gmail.com>
2022-08-19 00:16:59 +0600

    add burger

--- .history/static/js/main_20220819000821.js
+++
@@ -1,6 +0,0 @@
-const burger = document.getElementById('burger');
-const navLinks = document.querySelector('.custom-header-fixed');
-
-burger.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
